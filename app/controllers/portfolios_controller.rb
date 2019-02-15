3cdd86bcca15dca0f4ab88327839f7eca6e71910
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2019-02-15 22:07:29 +0200

    Implemented show functionality for portfolio modulde

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,9 +2,11 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
     end
+    
     def new
         @portfolio_item = Portfolio.new
     end
+
     def create
         @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
     
@@ -21,30 +23,20 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.find(params[:id])
     end
 
-    # def update
-    #     @portfolio_item = Portfolio.find(params[:id])
-
-    #     respond_to do |format|
-    #       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body)
-    #         format.html { redirect_to portfolios_path, notice: 'the record was successfully updated.' }
-    #       else
-    #         format.html { render :edit }
-    #       end
-    #     end
-    #   end
-
-
-      def update
-        @portfolio_item = Portfolio.find(params[:id])
-        puts @portfolio_item
-        respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-            format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-          else
-            format.html { render :edit }
-          end
+    def update
+      @portfolio_item = Portfolio.find(params[:id])
+      puts @portfolio_item
+      respond_to do |format|
+        if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+          format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        else
+          format.html { render :edit }
         end
       end
+    end
 
+    def show
+      @portfolio_item = Portfolio.find(params[:id])
+    end
 
 end
