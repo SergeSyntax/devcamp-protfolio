03f2ef2f04ea70c5abfdd4be418c24c88fe504f9
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2019-03-01 12:39:44 +0200

    Added controller concerns for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-
-  before_action :configure_premitted_parameters, if: :devise_controller?
-
-
-  def configure_premitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:acount_update, keys: [:name])
-
-  end
+include DeviseWhitelist
 end
